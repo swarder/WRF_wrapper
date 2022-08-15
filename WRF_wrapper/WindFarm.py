1bb869d0c95e51c98ae184cb8ce7c00494eb7b17
Simon Warder <s.warder15@imperial.ac.uk>
2022-08-15 11:58:19 +0100

    WindFarm can now be constructed using standard grid layout, with specified installed capacity

--- WRF_wrapper/WindFarm.py
+++ WRF_wrapper/WindFarm.py
@@ -1,9 +1,35 @@
 import pandas as pd
+import numpy as np
 import shutil
 import os
 import pkg_resources
 
 DATA_PATH = pkg_resources.resource_filename('WRF_wrapper', 'data/')
+
+class WindTurbine:
+    """
+    Object representing a wind turbine
+    This class is used to parse wind-turbine-<type_id>.tbl files
+    """
+    def __init__(self, tbl_file):
+        """
+        Initialise from tbl_file
+        """
+        f = open(tbl_file)
+        n = f.readline()
+        self.height, self.diameter, self.standing_thrust_coeff, self.nominal_power = [float(x) for x in f.readline().split()]
+        f.close()
+        self.power_curve = pd.read_csv(tbl_file, sep=' ', skiprows=2, names=['wind_speed', 'thrust_coeff', 'power'])
+
+        # Convert diameter to degrees
+        earth_radius = 6400000
+        self.diameter_degrees = self.diameter / earth_radius * 180 / np.pi
+
+    @classmethod
+    def from_type_id(cls, type_id):
+        tbl_file = os.path.join(DATA_PATH, f'wind-turbine-{type_id}.tbl')
+        return cls(tbl_file)
+
 
 class WindFarm:
     """
@@ -23,6 +49,36 @@ class WindFarm:
         if template_file is None:
             template_file = os.path.join(DATA_PATH, f'farm_template_{template_id}.txt')
         farm_df = pd.read_csv(template_file, names=['lat', 'lon', 'type_id'], delimiter=' ')
+        farm_df['lat'] = farm_df['lat'] - farm_df['lat'].mean() + farm_lat
+        farm_df['lon'] = farm_df['lon'] - farm_df['lon'].mean() + farm_lon
+        farm_df['type_id'] = type_id
+        return cls(farm_df)
+
+    @classmethod
+    def from_installed_capacity(cls, installed_capacity=5, layout='grid', farm_lon=0, farm_lat=0, type_id=6, turbine_spacing='5D'):
+        """
+        Create WindFarm object with specified installed_capacity (in MW)
+        WindFarm is constructed with a standard layout using turbines of specified type_id, and specified turbine_spacing
+        Array is constructed with the specified pattern until the installed capacity is met or exceeded
+        """
+        if not layout == 'grid':
+            raise NotImplemtedError
+
+        turbine = WindTurbine.from_type_id(type_id)
+
+        if isinstance(turbine_spacing, str):
+            # Assume of the form '7D', meaning spacing is 7 turbine diameters
+            assert turbine_spacing[-1] == 'D'
+            turbine_spacing = turbine.diameter_degrees * float(turbine_spacing[:-1])
+
+        # Generate simple turbine array
+        num_turbines = int(np.ceil(installed_capacity / turbine.nominal_power))
+        turbine_array_size = np.ceil(np.sqrt(num_turbines))
+        yy, xx = np.meshgrid(np.arange(turbine_array_size), np.arange(turbine_array_size))
+        turbine_xy = np.stack([xx.flatten(), yy.flatten()]).T[:num_turbines] * turbine_spacing
+        farm_df = pd.DataFrame(turbine_xy, columns=['lat', 'lon'])
+
+        # Now centre the farm in the correct place
         farm_df['lat'] = farm_df['lat'] - farm_df['lat'].mean() + farm_lat
         farm_df['lon'] = farm_df['lon'] - farm_df['lon'].mean() + farm_lon
         farm_df['type_id'] = type_id
--- examples/example.py
+++ examples/example.py
@@ -2,14 +2,15 @@ import datetime
 import os
 from WRF_wrapper import WRF_wrapper, WindFarm
 
-wf = WindFarm.WindFarm.from_template(template_id=0, farm_lon=0, farm_lat=1, type_id=6)
+wf1 = WindFarm.WindFarm.from_template(template_id=0, farm_lon=0, farm_lat=1, type_id=6)
+wf2 = WindFarm.WindFarm.from_installed_capacity(installed_capacity=90, farm_lon=2, farm_lat=1.5, type_id=6)
 
 config_dict = {'start_date': datetime.datetime(2000, 1, 4, 12),
                'end_date': datetime.datetime(2000, 1, 4, 15),
                'era_data_dir': '/home/scw15/wrf_test/era_data/2000/grb/',
                'pfile_data_dir': '/home/scw15/wrf_test/era_data/2000/PFILES',
                'geog_data_path': '/home/scw15/cx1_wrfwindpower/live/geog/WPS_GEOG/',
-               'wind_farms': [wf],
+               'wind_farms': [wf1, wf2],
                'WPS_path': '/home/scw15/wrf_test/WPS',
                'WRF_path': '/home/scw15/wrf_test/WRF',
                'wrf_img_path': '/home/scw15/wrf_test/wrf_sing',
