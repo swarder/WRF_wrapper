377597105f3e6b503f85862608464d745259ce57
Simon Warder <s.warder15@imperial.ac.uk>
2022-08-24 17:28:44 +0100

    Farm lat/lon need not be specified when loading from a template

--- WRF_wrapper/WindFarm.py
+++ WRF_wrapper/WindFarm.py
@@ -40,7 +40,7 @@ class WindFarm:
         self.farm_df = farm_df
 
     @classmethod
-    def from_template(cls, template_file=None, template_id=None, farm_lon=0, farm_lat=0, type_id=6):
+    def from_template(cls, template_file=None, template_id=None, farm_lon=None, farm_lat=None, type_id=6):
         """
         Create WindFarm object from a template file, specifying a new farm centre and turbine type
         User can either specify a filename containing the template layout (template_file)
@@ -49,8 +49,10 @@ class WindFarm:
         if template_file is None:
             template_file = os.path.join(DATA_PATH, f'templates/farm_template_{template_id}.txt')
         farm_df = pd.read_csv(template_file, names=['lat', 'lon', 'type_id'], delimiter=' ')
-        farm_df['lat'] = farm_df['lat'] - farm_df['lat'].mean() + farm_lat
-        farm_df['lon'] = farm_df['lon'] - farm_df['lon'].mean() + farm_lon
+        if farm_lat is not None:
+            farm_df['lat'] = farm_df['lat'] - farm_df['lat'].mean() + farm_lat
+        if farm_lon is not None:
+            farm_df['lon'] = farm_df['lon'] - farm_df['lon'].mean() + farm_lon
         farm_df['type_id'] = type_id
         return cls(farm_df)
 
