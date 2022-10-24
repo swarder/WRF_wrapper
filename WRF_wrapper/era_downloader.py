d4dc5970e2b5f814187728af39ca4a20b650d11e
Simon <s.warder15@imperial.ac.uk>
2022-10-24 16:31:41 +0100

    Fixing blocky SST interpolation which caused incorrect classification of ocean as ice

--- WRF_wrapper/era_downloader.py
+++ WRF_wrapper/era_downloader.py
@@ -28,10 +28,13 @@ def download_day(day, times):
         {
             "product_type": 'reanalysis',
             'variable': [
-            '10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
-            '2m_temperature', 'mean_sea_level_pressure', 'mean_wave_direction',
-            'mean_wave_period', 'sea_surface_temperature', 'significant_height_of_combined_wind_waves_and_swell',
-            'surface_pressure', 'total_precipitation',
+            '10m_u_component_of_wind','10m_v_component_of_wind','2m_dewpoint_temperature',
+            '2m_temperature','land_sea_mask','mean_sea_level_pressure',
+            'sea_ice_cover','sea_surface_temperature','skin_temperature',
+            'snow_depth','soil_temperature_level_1','soil_temperature_level_2',
+            'soil_temperature_level_3','soil_temperature_level_4','surface_pressure',
+            'volumetric_soil_water_layer_1','volumetric_soil_water_layer_2','volumetric_soil_water_layer_3',
+            'volumetric_soil_water_layer_4'
             ],
             'year': year[:4],
             'month': year[4:6],
@@ -47,15 +50,22 @@ def download_day(day, times):
             'product_type':'reanalysis',
             'variable': [
                 'geopotential', 'relative_humidity', 'temperature',
-                'u_component_of_wind', 'v_component_of_wind',
+                'u_component_of_wind', 'v_component_of_wind', 'specific_humidity',
              ],
             'pressure_level':[
-                '10','20','30','50','70','100',
-                '125','150','175','200','225','250',
-                '300','350','400','450','500','550',
-                '600','650','700','750','775','800',
-                '825','850','875','900','925','950',
-                '975','1000'
+                '1','2','3',
+                '5','7','10',
+                '20','30','50',
+                '70','100','125',
+                '150','175','200',
+                '225','250','300',
+                '350','400','450',
+                '500','550','600',
+                '650','700','750',
+                '775','800','825',
+                '850','875','900',
+                '925','950','975',
+                '1000'
             ],
             'year': year[:4],
             'month': year[4:6],
